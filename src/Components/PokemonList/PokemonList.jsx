c9ddf0fad7520c6eb5d8b68ef408c9bdd536246c
subhadip-kundu <subhadipkundu106@gmail.com>
2024-01-01 00:28:02 +0530

    Pagination added

--- src/Components/PokemonList/PokemonList.jsx
+++ src/Components/PokemonList/PokemonList.jsx
@@ -11,14 +11,27 @@ const PokemonList = () => {
     const [isLoading, setIsLoading] = useState(true);
 
     // URL for the Pokemon API
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon';
+    const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+
+    // Setting up next and previous button url
+    const [nextUrl, setNextUrl] = useState('');
+    const [prevUrl, setPrevUrl] = useState('');
 
     // Function to fetch Pokemon data from the API
     const fetchPokemonData = async () => {
         try {
+            // Get the loading page
+            setIsLoading(true);
+
             // Fetching the list of Pokemon from the API
-            const response = await axios.get(POKEDEX_URL);  // It download list of 20 pokemons
+            const response = await axios.get(pokedexUrl);  // It download list of 20 pokemons
             const pokemonResults = response.data.results;  // We get the array of pokemons from result
+
+
+            // Access next and previous data
+            setNextUrl(response.data.next);
+            setPrevUrl(response.data.previous);
+
 
             // Fetching details for each Pokemon in parallel
             const pokemonResultPromises = pokemonResults.map(pokemon => axios.get(pokemon.url));
@@ -47,7 +60,7 @@ const PokemonList = () => {
     // useEffect hook to trigger the fetchPokemonData function when the component mounts
     useEffect(() => {
         fetchPokemonData();
-    }, []);
+    }, [pokedexUrl]);
 
     // Render the component
     return (
@@ -62,8 +75,8 @@ const PokemonList = () => {
                 )}
             </div>
             <div className="controls-btn">
-                <button>Prev</button>
-                <button>Next</button>
+                <button disabled={prevUrl == null} onClick={() => { setPokedexUrl(prevUrl) }}>Prev</button>
+                <button disabled={nextUrl == null} onClick={() => { setPokedexUrl(nextUrl) }}>Next</button>
             </div>
         </div>
     );
